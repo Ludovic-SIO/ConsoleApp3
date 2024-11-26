d344c4e3f291344e5b8c40f61ffaf0780994ce92
lchamprobert <lchamprobert@N112-04>
2024-11-26 09:21:03 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static double ConvertBtToE (int a)
+        {
+            double b = 33803.1;
+            return a* b;
+
+        }
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Saisie le montant en Bitcoins");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Le montant est {0}",a);
+
+            while (a>= 0)
+            {
+                Console.WriteLine("Le montant en euros est {0}", ConvertBtToE(a));
+                Console.WriteLine("Saisie le montant en Bitcoins");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Le montant est {0}",a);
+            }
+               
+            Console.WriteLine("Le montant en euros est {0}",ConvertBtToE(a));
+            Console.ReadKey();
+        }
+    }
+}
